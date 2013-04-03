0890cd523439c6089fc3c196b5f573af009035cd
Ketan Deshmukh <ketan.davvindore@gmail.com>
2013-04-04 01:29:29 +0530

    added starter code,

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,18 @@
 class HomeController < ApplicationController
   def index
+	@ids = []
+	User.all.each {|user| @ids << user.id}
+  end
+
+  def get_user
+  	id = params[:user_id]
+
+  	# TODO 
+  	
+
+
+
+
+  	redirect_to root_path, :flash => { :success => "done"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 ClApp::Application.routes.draw do
   root :to => "home#index"
+
+  post "/home/get_user", :to => "home#get_user", as: "get_user"
 end
